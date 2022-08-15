45e80bdac8d07bf9dcbc7401682f9c69d592544f
joaovictor3g <jvdias79797@gmail.com>
2022-08-15 16:21:20 -0300

    feat: add http reqs

--- src/components/layout/Table/index.tsx
+++ src/components/layout/Table/index.tsx
@@ -1,36 +1,62 @@
+import { api } from "@/services/api";
+import { useEffect, useState } from "react";
 import { SearchForm } from "./SearchForm";
 import { Container, PriceHighlight, TransactionsTable } from "./styles";
 
+interface TransactionProps {
+  id: number;
+  description: string;
+  type: "income" | "outcome";
+  price: number;
+  createdAt: Date;
+  category: string;
+  priceFormatted?: string;
+  dateFormatted?: string;
+}
+
 export function Table() {
+  const [transactions, setTransactions] = useState<TransactionProps[]>([]);
+
+  async function loadTransactions() {
+    try {
+      const response = await api.get<TransactionProps[]>("/transactions");
+
+      const parsedTransactions = response.data.map((data) => ({
+        ...data,
+        priceFormatted: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }).format(data.price),
+        dateFormatted: new Intl.DateTimeFormat("pt-BR").format(
+          new Date(data.createdAt)
+        ),
+      }));
+
+      setTransactions(parsedTransactions);
+    } catch {}
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
   return (
     <Container>
       <SearchForm />
       <TransactionsTable>
         <tbody>
-          <tr>
-            <td width="50%">Desenvolvimento de site</td>
-            <td>
-              <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
-            </td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
-          <tr>
-            <td width="50%">Desenvolvimento de site</td>
-            <td>
-              <PriceHighlight variant="outcome"> - R$ 12.000,00</PriceHighlight>
-            </td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
-          <tr>
-            <td width="50%">Desenvolvimento de site</td>
-            <td>
-              <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
-            </td>
-            <td>Venda</td>
-            <td>13/04/2022</td>
-          </tr>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td width="50%">{transaction.description}</td>
+              <td>
+                <PriceHighlight variant={transaction.type}>
+                  {transaction?.priceFormatted ?? 0}
+                </PriceHighlight>
+              </td>
+              <td>{transaction.category}</td>
+              <td>{String(transaction?.dateFormatted ?? "No date")}</td>
+            </tr>
+          ))}
         </tbody>
       </TransactionsTable>
     </Container>
